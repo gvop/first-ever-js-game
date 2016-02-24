404a11a772b7c7ad0fec78f63ba8f9b5887aca9f
gvop <guusvanooijen@gmail.com>
2016-02-24 22:09:30 +0000

    Adding s to fonts

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php include_once('home.html'); ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-<script src="js/app.js"></script>
 <script src="js/turn.js"></script>
+<script src="js/app.js"></script>
